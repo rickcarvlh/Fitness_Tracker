a55cc0a73a592a5dad2524182319a802352ec8fb
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2019-02-25 15:41:37 +0000

    updating starter files because of some errors

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -1,9 +1,10 @@
 //  Our material Modules can all be managed from here
 import {NgModule} from "@angular/core";
+import {MatButtonModule} from "@angular/material";
 
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [MatButtonModule],
+  exports: [MatButtonModule]
 })
 export class MaterialModule {
 
