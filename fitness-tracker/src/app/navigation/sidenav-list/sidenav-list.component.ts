c0757f479f3adc75993ba8f488c3470c90a00b8a
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2019-02-28 18:57:10 +0000

    bug fixing related to routing / side bar

--- fitness-tracker/src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ fitness-tracker/src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,18 +1,24 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
 import {AuthService} from "../../auth/auth.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
  // listen from the outside
  @Output() sideNavClose = new EventEmitter<void>();
+ isAuth = false;
+ authSubscription: Subscription;
 
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
+   this.authSubscription=  this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
   }
 
   onClose() {
@@ -23,6 +29,10 @@ export class SidenavListComponent implements OnInit {
   onLogout() {
     this.authService.logout();
     this.onClose();
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
   }
 
 }
