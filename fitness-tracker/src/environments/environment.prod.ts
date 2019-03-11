e6b8ee6e74782b05c7c9334dac29769e4267a131
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2019-03-11 11:36:37 +0000

    Ready to send to production

--- fitness-tracker/src/environments/environment.prod.ts
+++ fitness-tracker/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAYkH1U002ESsjQ4FAzf70PYGUj3pN0Hp4',
+    authDomain: 'ng-fitness-tracker-8a1f6.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-8a1f6.firebaseio.com',
+    projectId: 'ng-fitness-tracker-8a1f6',
+    storageBucket: 'ng-fitness-tracker-8a1f6.appspot.com',
+    messagingSenderId: '158425320185'
+  }
 };
--- fitness-tracker/src/environments/environment.ts
+++ fitness-tracker/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: 'AIzaSyAYkH1U002ESsjQ4FAzf70PYGUj3pN0Hp4',
     authDomain: 'ng-fitness-tracker-8a1f6.firebaseapp.com',
